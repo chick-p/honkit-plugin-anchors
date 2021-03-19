2213eba8e98843730233376a838aebb6e1634e76
chick-p <chick.p.1120@gmail.com>
2021-03-19 16:27:02 +0900

    refactor: es5 -> es6+

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-var cheerio = require("cheerio");
-var slug = require("github-slugid");
+const cheerio = require("cheerio");
+const slug = require("github-slugid");
 
 // insert anchor link into section
 function insertAnchors(content) {
-  var $ = cheerio.load(content);
-  $(":header").each(function (i, elem) {
-    var header = $(elem);
-    var id = header.attr("id");
+  const $ = cheerio.load(content);
+  $(":header").each(function (_i, elem) {
+    const header = $(elem);
+    let id = header.attr("id");
     if (!id) {
       id = slug(header.text());
       header.attr("id", id);
     }
     header.prepend(
-      '<a name="' +
-        id +
-        '" class="plugin-anchor" ' +
-        'href="#' +
-        id +
-        '">' +
-        '<i class="fa fa-link" aria-hidden="true"></i>' +
-        "</a>"
+      `<a name="${id}" class="plugin-anchor" href="#${id}">
+        <i class="fa fa-link" aria-hidden="true"></i>
+      </a>`
     );
   });
   return $.html();
