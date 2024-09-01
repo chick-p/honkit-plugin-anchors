bf2f9defe7cc4ead7d164a54db2f8f66eaf5de34
piyo <chick.p.1120@gmail.com>
2024-09-01 21:04:38 +0900

    chore: migrate eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+    },
+  },
+];
