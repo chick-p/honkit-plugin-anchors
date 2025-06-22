d422163a5f9527256c8b80a40e1e0920f68e8f89
piyo <chick.p.1120@gmail.com>
2025-06-22 20:25:23 +0900

    test: add test

---
+++ index.test.js
@@ -0,0 +1,22 @@
+import path from "node:path";
+import { expect, test } from "vitest";
+
+import tester from "honkit-tester";
+
+const htmlContent = `<h1 id="title"><a name="title" class="plugin-anchor plugin-anchor--before" href="#title">
+                      <i class="fa fa-link before" aria-hidden="true"></i>
+                    </a>Title </h1>`;
+
+test("Generate anchor link", async () => {
+  const result = await tester
+    .builder()
+    .withContent("#Title {#title}")
+    .withLocalPlugin(path.join(__dirname, "."))
+    .withBookJson({
+      title: "Test Book",
+      plugins: ["anchors"],
+    })
+    .create();
+  const content = result.get("index.html").content;
+  expect(content).toEqual(htmlContent);
+});
